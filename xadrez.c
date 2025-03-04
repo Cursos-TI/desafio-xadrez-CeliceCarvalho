3d69d484e51aa8d208427721ced8a3340e7429a8
CeliceCarvalho <celicenunes22@gmail.com>
2025-03-04 17:34:02 -0400

    nivel novato

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,37 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    
+    int totCasasTorre = 5;
+    int totCasasBispo = 5;
+    int totCasasRainha = 8;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // char direcaoTorre[10] = "Direita";
+    // char direcaoBispo[20] = "Cima Direita";
+    // char direcaoRainha[10] = "Esquerda";
+    printf("Movimentacao da Torre\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    int casaTorre = 0; //inicialização da variável de controle da movimentação da Torre
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    while(casaTorre < totCasasTorre){//estrutura de repetição while que irá acontecer enquanto casaTorre for menor que o total
+        printf("Direita\n");
+        casaTorre ++;//incremento na variavel toda vez que a peça andar uma casa
+    }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("Movimentacao do Bispo\n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    int casaBispo = 0;//inicialização da variável de controle da movimentação do Bispo
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    do{//estrutura de repetição while que irá acontecer enquanto casaBispo for menor que o total
+        printf("Cima Direita\n");
+        casaBispo ++;//incremento na variavel toda vez que a peça andar uma casa
+    }while(casaBispo < totCasasBispo);
+
+    printf("Movimentacao da Rainha\n");
+
+    for(int casaRainha = 0; casaRainha < totCasasRainha; casaRainha++){//estrutura de repetição for (inicialização da variavel de controle;condiuçaõ de repetição;incremento)
+        printf("Esquerda\n");
+    }
 
     return 0;
 }
